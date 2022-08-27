11517391fe4ecf6f34f5b2884d33faaed04f4434
arthur-sudbrack-ibarra <arthur.sudbrack.ibarra@hp.com>
2022-08-27 02:11:26 -0300

    Grab logic progress.

--- src/components/Piano/Piano.tsx
+++ src/components/Piano/Piano.tsx
@@ -1,12 +1,51 @@
+import React from "react";
 import styles from "./Piano.module.css"
 import BlackKey from "../BlackKey/BlackKey";
 import WhiteKey from "../WhiteKey/WhiteKey";
 
-const intervals: number[] = [0, 1, 2]
+/* 
+    Draggable Piano Logic - Start
+*/
+interface DragInfo {
+    scrollLeft: number
+    mouseX: number
+}
+const pianoRef = React.createRef<HTMLDivElement>();
+const dragInfo: DragInfo = {
+    scrollLeft: 0,
+    mouseX: 0,
+}
+function mouseDownHandler(event: any) {
+    dragInfo.scrollLeft = event.target.scrollLeft
+    dragInfo.mouseX = event.clientX
+    window.addEventListener("mousemove", mouseMoveHandler)
+    window.addEventListener("mouseup", mouseUpHandler)
+    if (pianoRef.current) {
+        pianoRef.current.style.cursor = "grabbing"
+    }
+}
+function mouseMoveHandler(event: any) {
+    const distanceX = event.clientX - dragInfo.mouseX
+    if (pianoRef.current) {
+        pianoRef.current.scrollLeft = dragInfo.scrollLeft - distanceX
+    }
+}
+function mouseUpHandler() {
+    window.removeEventListener("mousemove", mouseMoveHandler)
+    window.removeEventListener("mouseup", mouseUpHandler)
+    if (pianoRef.current) {
+        pianoRef.current.style.cursor = "grab"
+    }
+}
+/* 
+    Draggable Piano Logic - End
+*/
+
+const intervals = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 function Piano() {
     return (
-        <div className={styles.piano} >
+        <div className={styles.piano} ref={pianoRef} onMouseDown={mouseDownHandler}>
             {intervals.map(interval => {
                 return (
                     <>
