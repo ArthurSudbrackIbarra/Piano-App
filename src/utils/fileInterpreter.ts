a5717fdacc8d3f76b38a07d944f2ae88f7392141
MagicManatee <arthur.ibarra@edu.pucrs.br>
2022-08-28 21:52:49 -0300

    tURKISH MARCH ALMOST DONE.

--- src/utils/fileInterpreter.ts
+++ src/utils/fileInterpreter.ts
@@ -20,20 +20,24 @@ export class PianoInterpreter {
   public async play(): Promise<void> {
     try {
       const lines = this.content.split("\n");
+      let hasStarted = false;
       for (const line of lines) {
         if (line.length === 0) {
           continue;
         }
         const splitted = line.split(/\(*\)( |\n)/);
         switch (true) {
-          case splitted[0].toUpperCase().startsWith("BPM"):
+          case splitted[0].toUpperCase().startsWith("START"):
+            hasStarted = true;
+            break;
+          case splitted[0].toUpperCase().startsWith("BPM") && hasStarted:
             {
               const parts = splitted[0].split(" ");
               const bpm = parseInt(parts[1]);
               this.bpm = bpm;
             }
             break;
-          case splitted[0].toUpperCase().startsWith("PAUSE"):
+          case splitted[0].toUpperCase().startsWith("PAUSE") && hasStarted:
             {
               const parts = splitted[0].split(" ");
               const seconds = parseInt(parts[1]) * 1000;
@@ -41,28 +45,32 @@ export class PianoInterpreter {
             }
             break;
           default: {
-            for (const noteData of splitted) {
-              if (noteData === " " || noteData.length === 0) {
-                continue;
+            if (hasStarted) {
+              for (const noteData of splitted) {
+                if (noteData === " " || noteData.length === 0) {
+                  continue;
+                }
+                const parts = noteData
+                  .replace("(", "")
+                  .replace(")", "")
+                  .split(" ");
+                const note = parts[0];
+                const duration = parseInt(parts[1]);
+                // Plays the note sound.
+                playNote(note, duration);
+                // Animates the keys.
+                document.getElementById(note)?.classList.add("active");
+                // Timeout to remove the animation.
+                setTimeout(() => {
+                  document.getElementById(note)?.classList.remove("active");
+                }, 20000 / this.bpm);
               }
-              const parts = noteData
-                .replace("(", "")
-                .replace(")", "")
-                .split(" ");
-              const note = parts[0];
-              const duration = parseInt(parts[1]);
-              // Plays the note sound.
-              playNote(note, duration);
-              // Animates the keys.
-              document.getElementById(note)?.classList.add("active");
-              // Timeout to remove the animation.
-              setTimeout(() => {
-                document.getElementById(note)?.classList.remove("active");
-              }, 20000 / this.bpm);
             }
           }
         }
-        await delay(60000 / this.bpm);
+        if (hasStarted) {
+          await delay(60000 / this.bpm);
+        }
       }
     } catch (error) {
       throw new Error("Error: Invalid '.piano' file.");
